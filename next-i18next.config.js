00e9f0511d87f80b0cd98a6ad0ef05ee35d58233
abdo <the.abdo.kh@gmail.com>
2025-07-16 18:24:06 +0300

    INIT

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -4,5 +4,8 @@ module.exports = {
         locales: ['en', 'ar'],
         localeDetection: true,
     },
+    localePath: typeof window === 'undefined'
+      ? require('path').resolve('./public/locales')
+      : '/locales',
     reloadOnPrerender: process.env.NODE_ENV === 'development',
 };
